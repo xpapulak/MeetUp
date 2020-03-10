41f4baed6a6ed1cf8938ef062b383a49e8b55543
xkovalc2 <xkovalc2@mendelu.cz>
2020-03-10 10:44:44 +0100

    firebase + lokacia

--- MeetUp/app/src/main/java/com/example/meetup/MapActivity.kt
+++ MeetUp/app/src/main/java/com/example/meetup/MapActivity.kt
@@ -1,19 +1,57 @@
 package com.example.meetup
 
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.graphics.BitmapFactory
+import android.location.Address
+import android.location.Geocoder
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.database.DatabaseError
+import com.google.firebase.database.ValueEventListener
+import com.karumi.dexter.Dexter
+import com.karumi.dexter.PermissionToken
+import com.karumi.dexter.listener.PermissionDeniedResponse
+import com.karumi.dexter.listener.PermissionGrantedResponse
+import com.karumi.dexter.listener.PermissionRequest
+import com.karumi.dexter.listener.single.PermissionListener
 
 import kotlinx.android.synthetic.main.activity_map.*
+import java.io.IOException
+import java.util.*
 
-class MapActivity : AppCompatActivity(), OnMapReadyCallback {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback, GoogleMap.OnMapClickListener, PermissionListener {
+
+    override fun onMapClick(p0: LatLng?) {
+
+    }
+
 
     private var googleMap: GoogleMap? = null
+    private lateinit var firebaseServer: FirebaseServer
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+
+    companion object {
+        const val REQUEST_CHECK_SETTINGS = 43
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,22 +60,153 @@ class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
         val mapFragment: SupportMapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
+        fusedLocationProviderClient = FusedLocationProviderClient(this)
 
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        firebaseServer = FirebaseServer(this) // inicializacia firebase databaze
+
     }
 
     override fun onMapReady(p0: GoogleMap?) {
         googleMap = p0
 
-        val markerOptions = MarkerOptions()
-        markerOptions.position(LatLng(15.0,45.toDouble()))
-        markerOptions.title("")
-        googleMap?.addMarker(markerOptions)
+        //val markerOptions = MarkerOptions()
+        //markerOptions.position(LatLng(15.0,45.toDouble()))
+        //markerOptions.title("")
+        //googleMap?.addMarker(markerOptions)
+
+        googleMap?.setOnMapClickListener(this)
+
+        if (isPermissionGiven()){
+            googleMap!!.isMyLocationEnabled = true
+            googleMap!!.uiSettings.isMyLocationButtonEnabled = true
+            googleMap!!.uiSettings.isZoomControlsEnabled = true
+            getCurrentLocation()
+        } else {
+            givePermission()
+        }
+
+
+
     }
+
+    private fun isPermissionGiven(): Boolean{
+        return ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun givePermission() {
+        Dexter.withActivity(this)
+            .withPermission(Manifest.permission.ACCESS_FINE_LOCATION)
+            .withListener(this)
+            .check()
+    }
+
+    override fun onPermissionGranted(response: PermissionGrantedResponse?) {
+        getCurrentLocation()
+    }
+
+    override fun onPermissionRationaleShouldBeShown(
+        permission: PermissionRequest?,
+        token: PermissionToken?
+    ) {
+        token!!.continuePermissionRequest()
+    }
+
+    override fun onPermissionDenied(response: PermissionDeniedResponse?) {
+        Toast.makeText(this, "Permission required for showing location", Toast.LENGTH_LONG).show()
+        finish()
+    }
+
+    private fun getCurrentLocation() {
+
+        val locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.interval = (10 * 1000).toLong()
+        locationRequest.fastestInterval = 2000
+
+        val builder = LocationSettingsRequest.Builder()
+        builder.addLocationRequest(locationRequest)
+        val locationSettingsRequest = builder.build()
+
+        val result = LocationServices.getSettingsClient(this).checkLocationSettings(locationSettingsRequest)
+        result.addOnCompleteListener { task ->
+            try {
+                val response = task.getResult(ApiException::class.java)
+                if (response!!.locationSettingsStates.isLocationPresent){
+                    getLastLocation()
+                }
+            } catch (exception: ApiException) {
+                when (exception.statusCode) {
+                    LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> try {
+                        val resolvable = exception as ResolvableApiException
+                        resolvable.startResolutionForResult(this, REQUEST_CHECK_SETTINGS)
+                    } catch (e: IntentSender.SendIntentException) {
+                    } catch (e: ClassCastException) {
+                    }
+
+                    LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> { }
+                }
+            }
+        }
+    }
+
+    private fun getLastLocation() {
+        fusedLocationProviderClient.lastLocation
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful && task.result != null) {
+                    val mLastLocation = task.result
+
+                    var address = "No known address"
+
+                    val gcd = Geocoder(this, Locale.getDefault())
+                    val addresses: List<Address>
+                    try {
+                        addresses = gcd.getFromLocation(mLastLocation!!.latitude, mLastLocation.longitude, 1)
+                        if (addresses.isNotEmpty()) {
+                            address = addresses[0].getAddressLine(0)
+                        }
+                    } catch (e: IOException) {
+                        e.printStackTrace()
+                    }
+
+                    val icon = BitmapDescriptorFactory.fromBitmap(BitmapFactory.decodeResource(this.resources, R.drawable.amu_bubble_shadow))
+                    val addMarker = googleMap?.addMarker(
+                        MarkerOptions()
+                            .position(LatLng(mLastLocation!!.latitude, mLastLocation.longitude))
+                            .title("Current Location")
+                            .snippet(address)
+                            .icon(icon)
+                    )
+
+                    val cameraPosition = CameraPosition.Builder()
+                        .target(LatLng(mLastLocation!!.latitude, mLastLocation!!.longitude))
+                        .zoom(17f)
+                        .build()
+                    googleMap?.moveCamera(CameraUpdateFactory.newCameraPosition(cameraPosition))
+                } else {
+                    Toast.makeText(this, "No current location found", Toast.LENGTH_LONG).show()
+                }
+            }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+
+        when (requestCode) {
+            REQUEST_CHECK_SETTINGS -> {
+                if (resultCode == Activity.RESULT_OK) {
+                    getCurrentLocation()
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data)
+
+    }
+
 
 }
 
