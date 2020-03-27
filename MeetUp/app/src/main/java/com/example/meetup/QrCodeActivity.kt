a0669bc08ee41c20dc70daf4ac0221c3a36415cb
xjantuli <xjantuli@mendelu.cz>
2020-03-27 04:30:22 +0100

    QR, Splash, Icon
    Pridaná launch ikona, splash screen a generovanie QR kódu ako aj náčrt zdielania -> zobrazenie tlačítka až po generovaní

--- MeetUp/app/src/main/java/com/example/meetup/QrCodeActivity.kt
+++ MeetUp/app/src/main/java/com/example/meetup/QrCodeActivity.kt
@@ -1,21 +1,42 @@
 package com.example.meetup
 
+import android.content.Context
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.MenuItem
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import net.glxn.qrgen.android.QRCode
 
 class QrCodeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
     lateinit var toolbar: Toolbar
     lateinit var drawerLayout: DrawerLayout
     lateinit var navView: NavigationView
+
+    //Pole na zadanie textu
+    private var editQRText: EditText? = null
+
+    //QR obrázok
+    private var imageQR: ImageView? = null
+
+    //tlačítko
+    private var createQR: Button? = null
+
+    //bitmapa
+    lateinit var bitmap: Bitmap
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -33,6 +54,38 @@ class QrCodeActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelec
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
         navView.setNavigationItemSelectedListener(this)
+
+        editQRText = findViewById<EditText>(R.id.editQRText)
+        imageQR = findViewById<ImageView>(R.id.qrcode)
+        createQR = findViewById<Button>(R.id.createQR)
+
+        //generovanie QR kódu
+        (createQR as Button).setOnClickListener {
+            val text = (editQRText as EditText).text.toString()
+
+            if (text.isEmpty()) {
+                Toast.makeText(this, "Nezadali ste žiadnu správu",
+                    Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
+            bitmap = QRCode.from(text).withSize(1000, 1000).bitmap()
+            (imageQR as ImageView).setImageBitmap(bitmap)
+            hideKeyboard()
+           var shareQr = findViewById<Button>(R.id.shareQR) as Button
+            shareQr.isClickable=true
+            shareQr.visibility= View.VISIBLE
+        }
+
+        //zdieľanie kódu
+    }
+
+    fun hideKeyboard() {
+        val view = this.currentFocus
+        if (view != null) {
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+            imm.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 
     override fun onNavigationItemSelected(item: MenuItem): Boolean {
